63d4b878629c6ad723c896d9d52e2223f6208394
derbuihan <derbuihan@gmail.com>
2023-08-10 21:38:07 +0900

    add unary & and *

--- parse.c
+++ parse.c
@@ -319,7 +319,7 @@ static Node *mul(Token **rest, Token *tok) {
     }
 }
 
-// unary = ("+" | "-")? primary
+// unary = ("+" | "-" | "*" | "&")? primary
 static Node *unary(Token **rest, Token *tok) {
     if (equal(tok, "+")) {
         return unary(rest, tok->next);
@@ -327,6 +327,14 @@ static Node *unary(Token **rest, Token *tok) {
 
     if (equal(tok, "-")) {
         return new_unary(ND_NEG, unary(rest, tok->next), tok);
+    }
+
+    if (equal(tok, "&")) {
+        return new_unary(ND_ADDR, unary(rest, tok->next), tok);
+    }
+
+    if (equal(tok, "*")) {
+        return new_unary(ND_DEREF, unary(rest, tok->next), tok);
     }
 
     return primary(rest, tok);
