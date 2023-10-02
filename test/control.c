b3625278a5b5541be152285e4a357f9728df2e0f
derbuihan <derbuihan@gmail.com>
2023-10-02 19:48:02 +0900

    allow for-loops to define local variables

--- test/control.c
+++ test/control.c
@@ -75,6 +75,18 @@ int main() {
         j;
     }));
 
+    ASSERT(55, ({
+        int j = 0;
+        for (int i = 0; i <= 10; i = i + 1) j = i + j;
+        j;
+    }));
+    ASSERT(3, ({
+        int i = 3;
+        int j = 0;
+        for (int i = 0; i < 10; i = i + 1) j = i + j;
+        i;
+    }));
+
     printf("OK\n");
     return 0;
 }
