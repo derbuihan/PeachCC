8bb65335f30a0ea710110dc4e07d4b6d345eee8b
derbuihan <derbuihan@gmail.com>
2023-10-13 17:47:53 +0900

    add && and ||

--- test/control.c
+++ test/control.c
@@ -87,6 +87,15 @@ int main() {
         i;
     }));
 
+    ASSERT(1, 0 || 1);
+    ASSERT(1, 0 || (2 - 2) || 5);
+    ASSERT(0, 0 || 0);
+    ASSERT(0, 0 || (2 - 2));
+
+    ASSERT(0, 0 && 1);
+    ASSERT(0, (2 - 2) && 5);
+    ASSERT(1, 1 && 5);
+
     printf("OK\n");
     return 0;
 }
